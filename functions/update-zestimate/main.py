d79aa166e62053b6447fc1f31da7168e1d20e49a
David F. Severski <davidski@deadheaven.com>
2017-04-05 12:46:11 -0700

    Linting per flake8

--- functions/update-zestimate/main.py
+++ functions/update-zestimate/main.py
@@ -13,6 +13,11 @@ from dateutil.parser import parse
 # set up logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 zpid = os.environ['zpid']
 zwsid = os.environ['zwsid']
@@ -29,15 +34,17 @@ def lambda_handler(event, context):
     data = pd.read_csv(obj['Body'], index_col=False)
 
     # fetch Zestimate
-    url = 'http://www.zillow.com/webservice/GetZestimate.htm?zws-id=' + zwsid + '&zpid=' + zpid + '&rentzestimate=TRUE'
+    url = 'http://www.zillow.com/webservice/GetZestimate.htm?zws-id=' + \
+          zwsid + '&zpid=' + zpid + '&rentzestimate=TRUE'
     response = requests.get(url)
 
-    if (response.status_code == 200):
-        logger.fatal("Received unexpected response (%s) from Zillow API" % response.status.code)
+    if response.status_code != 200:
+        logger.fatal("Received unexpected response (%s) from Zillow API" %
+                     response.status_code)
         return
 
     # parse response
-    soup = BeautifulSoup(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
     zestimate = soup.zestimate.amount.string
     last_updated = getattr(soup, 'last-updated').text
     zestimate_high = soup.zestimate.high.text
@@ -45,10 +52,12 @@ def lambda_handler(event, context):
     rent_zestimate = soup.rentzestimate.amount.text
     rent_high = soup.rentzestimate.high.text
     rent_low = soup.rentzestimate.low.text
+    logger.info("Zestimate as of %s: %s" % (last_updated, zestimate))
 
     # is last update of Zestimate > last date in CSV?
-    if (parse(last_updated) <== parse(data.iloc[-1, 0])):
-        logger.info("No update necessary. Exiting...")
+    if parse(last_updated) <= parse(data.iloc[-1, 0]):
+        logger.info("No update necessary - %s is not newer than %s. Exiting..." %
+                    (parse(last_updated), parse(data.iloc[-1, 0])))
         return
 
     # send message to SNS
@@ -59,10 +68,12 @@ def lambda_handler(event, context):
         Message='Current zestimate is ' + '${:,.2f}'.format(int(zestimate))
     )
 
-    logger.info("Sent message %s to topic %s" % (response['MessageId'], sns_topic_arn))
+    logger.info("Sent message %s to topic %s" %
+                (response['MessageId'], sns_topic_arn))
 
     # append Zestimate to data frame
-    data.loc[len(data)] = [last_updated, zestimate_high, zestimate, zestimate_low, rent_high, rent_zestimate, rent_low]
+    data.loc[len(data)] = [last_updated, zestimate_high, zestimate,
+                           zestimate_low, rent_high, rent_zestimate, rent_low]
 
     # write to S3
     csv_body = data.to_csv(None, index=False)
@@ -70,4 +81,5 @@ def lambda_handler(event, context):
 
 
 if __name__ == '__main__':
-    lambda_handler(event={'Records': [{'Sns': {'Message': 'foo'}}]}, context="")
+    lambda_handler(event={'Records': [{'Sns': {'Message': 'foo'}}]},
+                   context="")
